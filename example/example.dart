e00cbde3b2830937af0b560ea8b28dba453fdf04
f3ath <karapetov@gmail.com>
2024-06-01 21:21:56 -0700

    0.0.2

--- example/example.dart
+++ example/example.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:migrant/migrant.dart';
 import 'package:migrant/testing.dart';
 import 'package:migrant_db_sqlite/migrant_db_sqlite.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart' hide Database;
 
 Future<void> main() async {
   // These are the migrations. We are using a simple in-memory source,
